fe71218c7ed87c95a1253ceada81f166e9178408
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2024-11-12 11:26:10 +0530

    pythonBeginning

---
+++ python.py
@@ -0,0 +1,9 @@
+print("hello world!")
+
+my_tuple_1=(20,"cat",10) #have comma 
+my_list_4=list(my_tuple_1)
+my_list_4[1]="rat"
+my_tuple_1=tuple(my_list_4)
+print(my_tuple_1)
+print(type(my_tuple_1))
+
